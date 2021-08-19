338eb413aea93a8f0004c92790367fcb3901ea24
Josh Haver <jdhav2@gmail.com>
2021-08-18 22:45:32 -0400

    Second Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const { getSessions, getSessionByID, addOrUpdateSession, deleteSession } = require('./dynamo')
 require('dotenv').config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('./public'))
 app.use(express.json())
@@ -70,6 +70,6 @@ app.delete('/sessions/:id', async (req,res) =>{
     }
 })
 
-app.listen(PORT || 5000, () => {
+app.listen(8080, () => {
     console.log(`Listening on port ${PORT}`)
 })
